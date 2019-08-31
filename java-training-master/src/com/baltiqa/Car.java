8e26127b9d90dea610c624322ea7254851bfeb12
annuszka <annrybak92@gmail.com>
2019-08-31 11:45:28 +0200

    hello world project

---
+++ java-training-master/src/com/baltiqa/Car.java
@@ -0,0 +1,81 @@
+package com.baltiqa;
+
+public class Car {
+
+    private String make;
+    private String model;
+    private int year;
+    private String bodyType;
+    private String colour;
+    private int doors;
+    private String transmission;
+    private String fuelType;
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getBodyType() {
+        return bodyType;
+    }
+
+    public void setBodyType(String bodyType) {
+        this.bodyType = bodyType;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+        this.doors = doors;
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public void accelerate() {
+
+    }
+}
---
+++ java-training-master/src/com/baltiqa/Main.java
@@ -0,0 +1,9 @@
+package com.baltiqa;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//     write your code here
+    }
+}
